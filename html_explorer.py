dbe8b59f6eb983ea4db3c738b1fd65b98abea5f7
Andrés Ingelmo <andrw333@gmail.com>
2023-03-11 19:58:06 +0100

    Encapsulate code

--- html_explorer.py
+++ html_explorer.py
@@ -7,20 +7,20 @@ DATE = "20230903"
 DATA = Path("data")
 WORKING_DIR = DATA / DATE
 
-urls_list = []
 
+if __name__ == "__main__":
+    urls_list = []
+    for html in WORKING_DIR.glob("*.html"):
+        with open(html, "r") as file:
+            content = file.read()
 
-for html in WORKING_DIR.glob("*.html"):
-    with open(html, "r") as file:
-        content = file.read()
+        selector = Selector(text=content)
+        urls_in_html = selector.css("a[class='yYlJEf Q7PwXb L48Cpd brKmxb']").xpath("@href").getall()
 
-    selector = Selector(text=content)
-    urls_in_html = selector.css("a[class='yYlJEf Q7PwXb L48Cpd brKmxb']").xpath("@href").getall()
+        metro_area = html.stem.split("_")[0]
 
-    metro_area = html.stem.split("_")[0]
+        for url in urls_in_html:
+            urls_list.append((metro_area, url))
 
-    for url in urls_in_html:
-        urls_list.append((metro_area, url))
-
-urls_data = pd.DataFrame(urls_list, columns=["metro_area", "url"])
-urls_data.to_csv(DATA / "consolidated_urls.csv", index=False)
+    urls_data = pd.DataFrame(urls_list, columns=["metro_area", "url"])
+    urls_data.to_csv(DATA / "consolidated_urls.csv", index=False)
