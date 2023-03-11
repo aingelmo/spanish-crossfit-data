0c3627a9643bb397e4f57cd27bd9f8e8524b8b1a
Andrés Ingelmo <andrw333@gmail.com>
2023-03-11 19:56:18 +0100

    Remove dict way of storing

--- html_explorer.py
+++ html_explorer.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 
 import pandas as pd
@@ -8,7 +7,6 @@ DATE = "20230903"
 DATA = Path("data")
 WORKING_DIR = DATA / DATE
 
-urls_dict = {}
 urls_list = []
 
 
@@ -23,18 +21,6 @@ for html in WORKING_DIR.glob("*.html"):
 
     for url in urls_in_html:
         urls_list.append((metro_area, url))
-
-    try:
-        if urls_dict[metro_area]:
-            for url in urls_in_html:
-                urls_dict[metro_area].append(url)
-    except KeyError:
-        urls_dict[metro_area] = urls_in_html
-
-
-with open("urls.json", "w") as file:
-    json.dump(urls_dict, file, indent=4)
-
 
 urls_data = pd.DataFrame(urls_list, columns=["metro_area", "url"])
 urls_data.to_csv(DATA / "consolidated_urls.csv", index=False)
