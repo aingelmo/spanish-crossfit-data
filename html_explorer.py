b2dcb5ac241b76e54d631d78e3d612ee57219255
Andrés Ingelmo <andrw333@gmail.com>
2023-03-11 23:42:47 +0100

    Add csv url extractor

--- html_explorer.py
+++ html_explorer.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
@@ -5,22 +6,47 @@ from parsel import Selector
 
 DATE = "20230903"
 DATA = Path("data")
-WORKING_DIR = DATA / DATE
+WORKING_DIR = DATA / DATE / "raw"
+
+BOX_INFO = "div[class='VkpGBb']"
+BOX_NAME = "span[class='OSrXXb']::text"
+BOX_URL = "a[class='yYlJEf Q7PwXb L48Cpd brKmxb']"
+
+
+def get_content(html_file_path: Path) -> Selector:
+    """Opens up an html file and return the parsel selector ready to use."""
+    with open(html_file_path) as file:
+        html_content = file.read()
+        return Selector(text=html_content)
+
+
+def get_box_info_content(selector: Selector) -> list[str]:
+    """Returns all the information boxes present in the selector object."""
+    return selector.css(BOX_INFO).getall()
+
+
+def get_box_name_url(box_info_content: str) -> tuple[str, str]:
+    """Returns the box name and correspondent url from the information box."""
+    box_content = Selector(text=box_info_content)
+    box_name = box_content.css(BOX_NAME).get()
+    box_url = box_content.css(BOX_URL).xpath("@href").get()
+    return (box_name, box_url)
+
+
+def create_metro_box_data(box_info_list: list[tuple[str, str]], filename: str) -> None:
+    """Save a csv file with all the boxes names and urls from the correspondent metro area."""
+    metro_box_df = pd.DataFrame(box_info_list, columns=["box_name", "box_url"])
+    metro_box_df.to_csv(DATA / DATE / "csv" / f"{filename}_{datetime.now()}.csv", index=False)
 
 
 if __name__ == "__main__":
-    urls_list = []
-    for html in WORKING_DIR.glob("*.html"):
-        with open(html, "r") as file:
-            content = file.read()
+    for html_file in list(WORKING_DIR.glob("*.html")):
+        metro_area = html_file.stem.split("_")[0]
 
-        selector = Selector(text=content)
-        urls_in_html = selector.css("a[class='yYlJEf Q7PwXb L48Cpd brKmxb']").xpath("@href").getall()
+        main_file_selector = get_content(html_file)
 
-        metro_area = html.stem.split("_")[0]
+        boxes_with_content = get_box_info_content(main_file_selector)
 
-        for url in urls_in_html:
-            urls_list.append((metro_area, url))
+        box_info_list = [get_box_name_url(box_information) for box_information in boxes_with_content]
 
-    urls_data = pd.DataFrame(urls_list, columns=["metro_area", "url"])
-    urls_data.to_csv(DATA / "consolidated_urls.csv", index=False)
+        create_metro_box_data(box_info_list, metro_area)
